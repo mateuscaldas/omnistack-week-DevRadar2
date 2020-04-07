a7ec1eb1f7970c33c93822171ccfa7d336634006
Mateus Queiroz <mateus.caldas@hotmail.com>
2020-04-07 12:59:22 +0100

    last commit

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 //index, show, store, update, destroy
 
@@ -42,6 +44,16 @@ module.exports = {
                 techs: techsArray,
                 location,
             });
+
+            // Filtrar as conexões que estão a no máximo 10km de distância
+            // e que o novo dev tenha pelo menos uma das techs filtradas
+
+            const sendSocketMessageTo = findConnections(
+                {latitude, longitude},
+                techsArray
+            );
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }               
         return response.json(dev);        
     }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
-const cors = require('cors')
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://mateuscrqueiroz:mateuscrqueiroz@cluster0-nduru.mongodb.net/devRadar?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,5 +20,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
